72b8d18edca9ea0d5bfd2d03fc2e2ca553834f80
SAHIL511-JJ <sahillamture511@gmail.com>
2026-01-16 01:22:37 +0530

    Add Set A Q1: Student details program

---
+++ assignment1/set_a/setaq1.go
@@ -0,0 +1,20 @@
+// Program to print student details
+package main
+
+import "fmt"
+
+func main() {
+	// declaring variables for student info
+	var name string = "Sahil"
+	var rollno int = 101
+	var division string = "A"
+	var college string = "Modern College"
+
+	// printing student details
+	fmt.Println("Student Details")
+	fmt.Println("---------------")
+	fmt.Println("Name:", name)
+	fmt.Println("Roll No:", rollno)
+	fmt.Println("Division:", division)
+	fmt.Println("College:", college)
+}
